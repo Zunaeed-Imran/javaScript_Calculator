0431ffc65ab6e57042eb927d67ccdc4cea1771b7
imran <zunaeede@gmail.com>
2023-09-09 10:03:10 +0600

    main - basic number calculator in javascript

--- 2_number_calculator.js
+++ 2_number_calculator.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelectorAll('.input')
-    .forEach(element =>{
-        element.addEventListener('click',function(){
-            console.log(element);
+    const display = document.querySelector('.display');
+    const buttons = document.querySelectorAll('.buttons');
+    const equal = document.querySelector('.equal');
+    buttons.forEach(element => {
+        element.addEventListener('click', function(){
+            display.textContent += element.textContent;
+        });
+    });
+    equal.addEventListener('click', function(){
+        const inputs = display.textContent.split('');
+        inputs.forEach((input, index) => {
+            if(input === '+'){
+                const num1 = Number(inputs[index - 1]);
+                const num2 = Number(inputs[index + 1]);
+                const calc = num1 + num2;
+                console.log(calc);
+                alert(calc);
+            }
         })
     })
 })
