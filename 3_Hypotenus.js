565dfcb723ebc9cc28b0c81c5d1ac34fd7a1469e
imran <zunaeede@gmail.com>
2023-10-01 09:27:04 +0600

    main - done task no 3 - hypotenus or triangle

--- 3_Hypotenus.js
+++ 3_Hypotenus.js
@@ -1,13 +1,16 @@
+// this program will show the Hypotenus result.
+
+
 let a;
 let b;
 let c;
 
 document.getElementById('submitButton').onclick = function () {
   a = document.getElementById('aTextBox').value;
-  a = Number(a);
+  a = parseInt(a);
 
   b = document.getElementById('bTextBox').value;
-  b = Number(b);
+  b = parseInt(b);
 
   c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 
