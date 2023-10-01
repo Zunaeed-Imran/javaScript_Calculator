f236412edff9455a1344ef6d938e08d875915848
imran <zunaeede@gmail.com>
2023-10-01 11:22:40 +0600

    note - practice some on hypotenus calculator

--- 1_practice.js
+++ 1_practice.js
@@ -2,4 +2,14 @@ let a;
 let b;
 let c;
 
-document.getElementById()
+document.getElementById('button').onclick = function () {
+  a = document.getElementById('A_input').value;
+  a = parseInt(a);
+
+  b = document.getElementById('B_input').value;
+  b = parseInt(a);
+
+  c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+  document.getElementById('result_C').innerHTML = 'The rasult is' + c;
+}
